b3e63c07e097a5da37912a5218e88d78fee1f7b8
John Sloan <jsloan256@gmail.com>
2015-08-11 22:43:42 -0500

    Added all wiggle ports to the testbench

--- titan_wiggle/source/wiggle_tb.v
+++ titan_wiggle/source/wiggle_tb.v
@@ -3,10 +3,26 @@
 module wiggle_tb ();
 
 reg osc;
-reg perstn;
-
 wire [31:0] gpio_a;
 wire [31:0] gpio_b;
+reg perstn;
+wire refclkp, refclkn, hdinp0, hdinn0;
+wire hdoutp0, hdoutn0;
+
+wire ddr3_rstn;
+wire ddr3_ck0;
+wire ddr3_cke;
+wire [13:0] ddr3_a;
+wire [2:0] ddr3_ba;
+wire [15:0] ddr3_d;
+wire [1:0] ddr3_dm;
+wire [1:0] ddr3_dqs;
+wire ddr3_csn;
+wire ddr3_casn; 
+wire ddr3_rasn;
+wire ddr3_wen;
+wire ddr3_odt;
+
 
 // --------------------------------------------------------------------
 // Power on Reset
@@ -17,7 +33,26 @@ GSR     GSR_INST(.GSR(1'b1));
 wiggle U1 ( .osc(osc),
 				.gpio_a(gpio_a),
 				.gpio_b(gpio_b),
-				.perstn(perstn)
+				.perstn(perstn),
+				.refclkp(refclkp),
+				.refclkn(refclkn),
+				.hdinp0(hdinp0),
+				.hdinn0(hdinn0),
+				.hdoutp0(hdoutp0),
+				.hdoutn0(hdoutn0),
+				.ddr3_rstn(ddr3_rstn),
+				.ddr3_ck0(ddr3_ck0),
+				.ddr3_cke(ddr3_cke),
+				.ddr3_a(ddr3_a),
+				.ddr3_ba(ddr3_ba),
+				.ddr3_d(ddr3_d),
+				.ddr3_dm(ddr3_dm),
+				.ddr3_dqs(ddr3_dqs),
+				.ddr3_csn(ddr3_csn),
+				.ddr3_casn(ddr3_casn),
+				.ddr3_rasn(ddr3_rasn),
+				.ddr3_wen(ddr3_wen),
+				.ddr3_odt(ddr3_odt)
 			);
 
 always
